6f3e1f74e5ad1504bf2fd490dabe5efc92f70609
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-10-19 16:01:59 -0400

    Added middleware to throttle down requests

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 require("dotenv").config();
 
-import express, { urlencoded } from "express";
+import express, { Request, Response, urlencoded } from "express";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 // Routers
 import NotesRoutes from "./Controllers/Notes/NotesRoutes";
@@ -10,9 +11,22 @@ import error from "./utils/middleware/errors";
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20, // Limit each IP to 20 requests per `window` (here, per 1 minute)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: async (req: Request, res: Response) => {
+    return res
+      .status(429)
+      .json({ status: "Error", message: "Request limit reached" });
+  },
+});
+
 app.use(cors());
 app.use(urlencoded({ extended: true }));
 app.use(express.json());
+app.use(limiter);
 
 app.use(NotesRoutes);
 app.use(error);
