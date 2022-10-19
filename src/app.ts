a1933c41ed1baacc7291c926d327fc2e6d5307f7
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-10-19 16:06:13 -0400

    Changed to 50 request per minute max

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 20, // Limit each IP to 20 requests per `window` (here, per 1 minute)
+  max: 50, // Limit each IP to 20 requests per `window` (here, per 1 minute)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: async (req: Request, res: Response) => {
